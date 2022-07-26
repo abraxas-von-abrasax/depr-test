2ee5a079e48428640b678852a8e55c8ca7210176
Nenad Petkovic <root@nenad-petkovic.com>
2022-07-26 20:45:50 +0200

    add reproducible intellisense issue

---
+++ packages/main/src/main.ts
@@ -0,0 +1,9 @@
+import { Wrapper } from '@depr-test/wrapper';
+
+main();
+
+function main() {
+    const lib = Wrapper.lib;
+    lib.f();
+    lib.g();
+}
---
+++ packages/wrapper/src/wrapper.ts
@@ -0,0 +1,5 @@
+import { Lib } from '@depr-test/lib';
+
+export class Wrapper {
+    static lib = Lib;
+}
